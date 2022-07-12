8379c1f00dcf497e2a23da210def9c615a8bd652
Madhushika123 <96645@fhss.sjp.ac.lk>
2022-07-12 04:48:53 -0700

    Add main topic in the home page

--- Frontend/src/components/header/Header.jsx
+++ Frontend/src/components/header/Header.jsx
@@ -4,12 +4,12 @@ export default function Header() {
   return (
     <div className="header">
       <div className="headerTitles">
-        <span className="headerTitleSm">React & Node</span>
+        <span className="headerTitleSm">Animals Lover</span>
         <span className="headerTitleLg">Blog</span>
       </div>
       <img
         className="headerImg"
-        src="https://i.pinimg.com/736x/51/74/64/517464955f9bbc5400bf59f8b33eecfa--desktop-wallpapers-hd-wallpaper.jpg"
+        src="https://media.istockphoto.com/photos/large-group-of-african-safari-animals-wildlife-conservation-concept-picture-id1209602332?k=20&m=1209602332&s=612x612&w=0&h=1kD50LQUUoE0bV0Cnt2ChHeY_E7_IPIxazTAspo8y1g="
         alt=""
       />
     </div>
