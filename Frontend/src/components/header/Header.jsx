93904bd8086618e84c536ac6ee073744ae9a5c85
Madhushika123 <96645@fhss.sjp.ac.lk>
2022-07-20 11:11:05 -0700

    final commit

--- Frontend/src/components/header/Header.jsx
+++ Frontend/src/components/header/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
     <div className="header">
       <div className="headerTitles">
         <span className="headerTitleSm">Animals Lover</span>
-        <span className="headerTitleLg">Blog</span>
+        <span className="headerTitleLg">Blog </span>
       </div>
       <img
         className="headerImg"
