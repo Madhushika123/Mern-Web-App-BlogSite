3eac8945205f5827bf83e98ec5f5bec6276041c9
Madhushika123 <96645@fhss.sjp.ac.lk>
2022-07-19 21:15:35 -0700

    Remove to delete account title

--- Frontend/src/pages/settings/Settings.jsx
+++ Frontend/src/pages/settings/Settings.jsx
@@ -46,7 +46,7 @@ export default function Settings() {
       <div className="settingsWrapper">
         <div className="settingsTitle">
           <span className="settingsUpdateTitle">Update Your Account</span>
-          <span className="settingsDeleteTitle">Delete Account</span>
+          <span className="settingsDeleteTitle"></span>
           <button className="settingsDeleteSubmit" type="submit">
             Delete
           </button>
